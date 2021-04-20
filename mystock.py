08511c5a7607cb9c38ccb76da5860a3c28531f91
mikhp <mikhp@qq.com>
2021-04-20 17:41:13 +0800

    sma testing

--- mystock.py
+++ mystock.py
@@ -11,16 +11,19 @@ class SmaCross(bt.Strategy):
     def __init__(self):
         sma1 = bt.ind.SMA(period=self.p.pfast)
         sma2 = bt.ind.SMA(period=self.p.pslow)
-        self.crossover= bt.ind.CrossOver(sma1,sma2)
+        # self.crossover= bt.ind.CrossOver(sma1,sma2)
+        self.crossover = bt.ind.CrossOver(sma1, sma2)  # crossover signal
     
     def next(self):
         if not self.position:
             if self.crossover>0:
-                self.buy()
-        elif self.crossover<0:
+                self.buy()               
+        elif self.crossover<0:            
             self.close()
 
 cerebro= bt.Cerebro()
+cerebro.broker.setcash(100000)
+cerebro.broker.setcommission(commission=0.003)
 # 通过getdata.py获取股票数据文件cvs
 data = bt.feeds.YahooFinanceCSVData(dataname="./600999.sh.csv")
 cerebro.adddata(data)
