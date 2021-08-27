8a111732171c04f59c57641c57e8f82cddb06ca5
mikhp <mikhp@qq.com>
2021-08-27 15:25:18 +0800

    新增策略

--- btrader.py
+++ btrader.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-from __future__ import (absolute_import, division, print_function,unicode_literals)
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 
 import datetime  # For datetime objects
 import os.path  # To manage paths
-import sys  # To find out the script name (in argv[0])
+import sys
+from tkinter.constants import S  # To find out the script name (in argv[0])
 import backtrader as bt
 
 
@@ -28,7 +30,9 @@ class TestStrategy(bt.Strategy):
             self.datas[0], period=self.params.maperiod)
         # Indicators for the plotting show
         bt.indicators.ExponentialMovingAverage(self.datas[0], period=25)
-        bt.indicators.WeightedMovingAverage(self.datas[0], period=25, subplot=True)
+        bt.indicators.WeightedMovingAverage(self.datas[0],
+                                            period=25,
+                                            subplot=True)
         bt.indicators.StochasticSlow(self.datas[0])
         bt.indicators.MACDHisto(self.datas[0])
         rsi = bt.indicators.RSI(self.datas[0])
@@ -44,8 +48,9 @@ class TestStrategy(bt.Strategy):
         # Attention: broker could reject order if not enough cash
         if order.status in [order.Completed]:
             if order.isbuy():
-                self.log('BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' % (
-                    order.executed.price, order.executed.value, order.executed.comm))
+                self.log('BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                         (order.executed.price, order.executed.value,
+                          order.executed.comm))
 
                 self.buyprice = order.executed.price
                 self.buycomm = order.executed.comm
@@ -99,6 +104,58 @@ class TestStrategy(bt.Strategy):
                 self.order = self.sell()
 
 
+class kdjStrategy(bt.Strategy):
+    params = dict(
+        fast_ma=2,
+        slow_ma=5,
+        rsi_period =5,
+        rsi_high=60,
+        rsi_low=50,
+    )
+
+    def __init__(self):
+        self.order = None
+        self.sma = bt.indicators.MovingAverageSimple(period=self.p.fast_ma)
+        self.sma2 = bt.indicators.MovingAverageSimple(period=self.p.slow_ma)
+        self.rsi = bt.indicators.RSI(period=self.p.rsi_period)
+        # self.dataclose = self.datas[0].close
+        # fast_ma = bt.ind.EMA(period=self.p.fast_ma)
+        # slow_ma = bt.ind.EMA(period=self.p.slow_ma)
+        self.crossover = bt.ind.CrossOver(self.sma,self.sma2)
+      
+        
+
+        # print(self.datas[0])
+    def log(self, txt, dt=None):
+        ''' Logging function fot this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def next(self):
+
+        if self.order:
+            return
+
+        if self.crossover < 0 and self.rsi < self.p.rsi_low:
+            self.order = self.buy()
+            
+        elif self.crossover > 0 and self.rsi > self.p.rsi_high:
+            self.order = self.sell()
+            
+            
+
+        self.order = None
+
+    def notify_order(self, order):
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log('买入, %.2f' % order.executed.price)
+            elif order.issell():
+                self.log('卖出, %.2f' % order.executed.price)
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("需要stockid，如：600031.sh或者300348.sz")
@@ -110,7 +167,7 @@ if __name__ == '__main__':
     cerebro = bt.Cerebro()
 
     # Add a strategy
-    cerebro.addstrategy(TestStrategy)
+    cerebro.addstrategy(kdjStrategy)
 
     # Datas are in a subfolder of the samples. Need to find where the script is
     # because it could have been called from anywhere
@@ -133,13 +190,14 @@ if __name__ == '__main__':
     cerebro.adddata(data)
 
     # Set our desired cash start
-    cerebro.broker.setcash(1000.0)
+    startmoney = 70000.0
+    cerebro.broker.setcash(startmoney)
 
     # Add a FixedSize sizer according to the stake
     cerebro.addsizer(bt.sizers.FixedSize, stake=10)
 
     # Set the commission
-    cerebro.broker.setcommission(commission=0.0)
+    cerebro.broker.setcommission(commission=0.003)
 
     # Print out the starting conditions
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
@@ -148,7 +206,11 @@ if __name__ == '__main__':
     cerebro.run()
 
     # Print out the final result
-    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    print('最终市值: %.2f' % cerebro.broker.getvalue())
+    allmoney = cerebro.broker.getvalue() - startmoney
+    print('最终盈利: %.2f' % allmoney)
+    ab = float(allmoney / startmoney * 100)
+    print('盈亏率: %.2f' % ab + '%')
 
     # Plot the result
     cerebro.plot()
